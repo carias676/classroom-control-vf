efd3c3d88b74f0e195515aa3c1227b6d01cd48f9
carias676 <carias676@gmail.com>
2016-04-25 16:18:28 -0700

    Managing /etc/motd

--- manifests/site.pp
+++ manifests/site.pp
@@ -44,11 +44,7 @@ node default {
   #   class { 'my_class': }
   notify { "Hello, my name is ${::hostname}": }
   
-  file { '/etc/motd':
-     ensure => file,
-     owner => 'root',
-     group => 'root',
-     mode => '0644',
-     content => "I'm doing my best.\n",
+  exec { "cowsay 'Welcome to ${::fqdn}!' > /etc/motd":
+    creates => '/etc/motd',
   }
 }
